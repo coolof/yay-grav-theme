0d55ead9b54c8dba34cbc70dc628beb4db511e1c
Olof Brickarp <olof@understandit.se>
2015-11-16 09:34:02 +0100

    Bio, various small updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
   $('p:has(img)').addClass('p-img');
+
+  $('p:has(img)').each(function() {
+    var imgClass = $(this).find('img').attr('class');
+
+    console.log(imgClass);
+    $(this).addClass('p-img ' + imgClass);
+  });
 });
